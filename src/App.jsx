b8d0766cbf6a65a9ea98d7063c105789572f1a26
Ardonit Saliji <ardonit.1980@gmail.com>
2022-06-26 16:12:28 +0200

    react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+
+function App() {
+  return (
+    <div className='container'>
+      <Navbar />
+    </div>
+  )
+}
+export default App
---
+++ src/component/Footer.jsx
@@ -0,0 +1,10 @@
+import '../style.css';
+const Footer = () => {
+  return (
+    <div>
+      <footer> &copy; 2022 Ardonit. All rights reserved</footer>
+    </div>
+  );
+};
+
+export default Footer;
